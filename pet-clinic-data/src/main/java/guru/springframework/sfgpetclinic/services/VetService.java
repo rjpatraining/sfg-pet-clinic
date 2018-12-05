fc9f84c88d2922f50e46a95c99dd644705793050
Rajmohan P <rajmohan.p@tavant.com>
2018-12-05 07:42:36 -0600

    Create Base Interface and refactor the service classes to extend the base interface. Closes #11

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/VetService.java
@@ -2,13 +2,6 @@ package guru.springframework.sfgpetclinic.services;
 
 import guru.springframework.sfgpetclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long> {
 
-public interface VetService {
-
-    Vet findbyId(Long Id);
-
-    Vet save(Vet owner);
-
-    Set<Vet> findAll();
 }
