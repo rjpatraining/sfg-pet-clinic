2ac513ce6ecd6701b550edfe988c265f2dcf82ba
Rajmohan P <rajmohan.p@tavant.com>
2019-08-23 15:29:21 -0500

    Created Services for PetType. Closes #43

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/PetTypeRepository.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/PetTypeRepository.java
@@ -1,7 +1,7 @@
 package guru.springframework.sfgpetclinic.repositories;
 
-import guru.springframework.sfgpetclinic.model.Pet;
+import guru.springframework.sfgpetclinic.model.PetType;
 import org.springframework.data.repository.CrudRepository;
 
-public interface PetTypeRepository extends CrudRepository<Pet, Long> {
+public interface PetTypeRepository extends CrudRepository<PetType, Long> {
 }
