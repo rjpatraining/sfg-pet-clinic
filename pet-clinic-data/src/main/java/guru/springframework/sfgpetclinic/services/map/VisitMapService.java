67aea6099b24248b6c9b37c9507fd231432f6854
Rajmohan P <rajmohan.p@tavant.com>
2019-08-28 08:10:50 -0500

    Created Visit Map Service Closes #47

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/VisitMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/VisitMapService.java
@@ -28,7 +28,7 @@ public class VisitMapService extends AbstractMapService<Visit, Long> implements 
         if(visit.getPet()==null || visit.getPet().getOwner() == null || visit.getPet().getId() == null || visit.getPet().getOwner().getId() == null){
             throw new RuntimeException("Invalid Visit");
         }
-        return super.save(object);
+        return super.save(visit);
     }
 
     @Override
